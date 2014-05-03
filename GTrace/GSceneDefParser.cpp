e2700f37c71c65b76f8bb301698e26eebbebdaf6
Pilisi Gergo <pilisi.gergo@gmail.com>
2014-05-03 19:29:00 +0200

    Implemented basic scene definition parsing. Added sample scene file.

--- GTrace/GSceneDefParser.cpp
+++ GTrace/GSceneDefParser.cpp
@@ -7,7 +7,9 @@
 #include <boost/fusion/include/std_pair.hpp>
 #include"GSceneDefParser.h"
 #include<boost/fusion/include/adapt_struct.hpp>
-
+#include <boost/spirit/include/phoenix_operator.hpp>
+#include <boost/spirit/include/phoenix.hpp>
+#include <Eigen/core>
 /* Scene definition grammar
 
   Scene: Object+
@@ -28,12 +30,23 @@ enum ObjectType {
 	Light, Plane, Sphere, Camera
 };
 
+struct Vec {
+	float x, y, z;
+};
+
+BOOST_FUSION_ADAPT_STRUCT(
+	Vec,
+	(float, x)
+	(float, y)
+	(float, z)
+)
+
 struct ObjectDefinition {
 	ObjectType type;
 	std::string name;
-	std::map<std::string, std::string> attributes;
+	std::map<std::string, boost::any> attributes;
 };
-typedef std::map<std::string, std::string> attr_t;
+typedef std::map<std::string, boost::any> attr_t;
 
 BOOST_FUSION_ADAPT_STRUCT(
 	ObjectDefinition,
@@ -53,40 +66,50 @@ struct object_type : qi::symbols<char, ObjectType> {
 }object_type;
 
 template <typename Iterator>
-struct scenedef_parser : qi::grammar<Iterator,  ObjectDefinition(), ascii::space_type>
+struct scenedef_parser : qi::grammar<Iterator,  std::vector<ObjectDefinition>(), ascii::space_type>
 {
 	scenedef_parser() : scenedef_parser::base_type(start)
 	{
 		using qi::int_;
 		using qi::lit;
-		using qi::double_;
+		using qi::float_;
 		using qi::lexeme;
 		using ascii::char_;
 		using qi::alpha;
 		using qi::alnum;
+		using qi::_val;
+		using qi::_1;
+		using qi::_2;
+		using qi::int_;
+		using boost::phoenix::construct;
 
-		object = object_type >> identifier >> map;
-		quoted_string %= lexeme['"' >> +(char_ - '"') >> '"'];
 		identifier %= +alpha >> *alnum;
-		keyValue = identifier >> ':' >> quoted_string;
-		map = '{' >> (keyValue % ',') >> '}';
-		start = object;
+		quoted_string %= lexeme['"' >> +(char_ - '"') >> '"'];
+		vector = '(' >> float_ >> ',' >> float_ >> ',' >> float_ >> ')';
+		keyValue = identifier >> ':' >> value;
+		value = float_ | vector | identifier | map;
+		map = '{' >> (+keyValue) >> '}';
+		object = object_type >> identifier >> map;
+		start = +object;
 	}
 
-	qi::rule<Iterator, std::map<std::string, std::string>(), ascii::space_type> map;
-	qi::rule<Iterator, std::pair<std::string, std::string>(), ascii::space_type> keyValue;
+
+	qi::rule<Iterator, Vec(), ascii::space_type> vector;
+	qi::rule<Iterator, std::map<std::string, boost::any>(), ascii::space_type> map;
+	qi::rule<Iterator, std::pair<std::string, boost::any>(), ascii::space_type> keyValue;
 	qi::rule<Iterator, std::string(), ascii::space_type> quoted_string;
 	qi::rule<Iterator, std::string(), ascii::space_type> identifier;
+	qi::rule<Iterator, boost::any(), ascii::space_type> value;
 	qi::rule<Iterator, ObjectDefinition(), ascii::space_type> object;
-	qi::rule<Iterator, ObjectDefinition(), ascii::space_type> start;
+	qi::rule<Iterator, std::vector<ObjectDefinition>(), ascii::space_type> start;
 };
 
-int parser_test() 
+int parser_test()
 {
 	scenedef_parser<std::string::iterator> parser;
-	std::string def("Camera cam01 { pos:\"xyz\", orientation:\"342\"}");
-	boost::any value;
-	boost::spirit::qi::phrase_parse(def.begin(), def.end(), parser, boost::spirit::ascii::space, value);
+	std::string def("Camera cam01 { pos: (0,0,0) orientation:(0,0,-1) }");
+	std::vector<ObjectDefinition> objects;
+	bool result = boost::spirit::qi::phrase_parse(def.begin(), def.end(), parser, boost::spirit::ascii::space, objects);
 	
 	return 0;
 }
